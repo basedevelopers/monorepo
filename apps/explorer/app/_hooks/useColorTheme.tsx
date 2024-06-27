dc731fbfc9f1f5e63883d74e84823b5ab9ed75aa
basedotdev <a@base.dev>
2024-06-27 18:03:42 +0900

    fix

--- apps/explorer/app/_hooks/useColorTheme.tsx
+++ apps/explorer/app/_hooks/useColorTheme.tsx
@@ -1,4 +1,4 @@
-import { useLocalStorage } from "@uidotdev/usehooks"
+import useLocalStorage from "@/hooks/useLocalStorage"
 
 export const COLOR_THEMES = [
   "blue",
@@ -10,11 +10,16 @@ export const COLOR_THEMES = [
   "rose",
 ]
 
+const DEFAULT_COLOR_THEME = COLOR_THEMES[0]
+
 export const useColorTheme = () => {
-  const [colorTheme, setColorTheme] = useLocalStorage("color-theme", "blue")
+  const [colorTheme, setColorTheme] = useLocalStorage(
+    "color-theme",
+    DEFAULT_COLOR_THEME,
+  )
 
   const toggle = () => {
-    const currentIndex = COLOR_THEMES.indexOf(colorTheme)
+    const currentIndex = COLOR_THEMES.indexOf(colorTheme ?? DEFAULT_COLOR_THEME)
     const nextIndex = (currentIndex + 1) % COLOR_THEMES.length
     setColorTheme(COLOR_THEMES[nextIndex])
   }
