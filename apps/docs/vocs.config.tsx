8a6bd6edd5ba52b2764b84626949e6ad7f24d137
basedotdev <a@base.dev>
2024-06-30 14:01:57 +0900

    temp

--- apps/docs/vocs.config.tsx
+++ apps/docs/vocs.config.tsx
@@ -26,6 +26,37 @@ export default defineConfig({
       link: "https://twitter.com/wevm_dev",
     },
   ],
+  head({ path }) {
+    // if (path === "/docs") return <></>
+    return (
+      <>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </>
+    )
+  },
   // logoUrl: `https://raw.githubusercontent.com/base-org/brand-kit/main/logo/in-product/Base_Network_Logo.svg`,
   // iconUrl: `https://raw.githubusercontent.com/base-org/brand-kit/main/logo/in-product/Base_Network_Logo.svg`,
 })
